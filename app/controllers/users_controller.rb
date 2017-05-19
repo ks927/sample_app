706920217b2815fd35346e5e21ec07a62009d277
KSheehan <ksheehan927@gmail.com>
2017-05-18 20:50:24 -0400

    Complete flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
      @user = User.new(user_params)
       if @user.save
-          redirect_to new_user_path
+          flash[:success] = "Welcome to the Sample App!"
+          redirect_to @user
       else
           render :new
       end
