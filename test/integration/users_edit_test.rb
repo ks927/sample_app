b8b8b2ce437ff57f67e80797512e1bc4cac9c2e8
KSheehan <ksheehan927@gmail.com>
2017-05-30 17:18:27 -0400

    Add user index page with pagination

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -14,7 +14,7 @@ class UsersEditTest < ActionDispatch::IntegrationTest
        assert_select "div.alert", "The form contains 4 errors."
     end
     
-    test "succesfful edit" do
+    test "succesful edit" do
        log_in_as(@user)
        get edit_user_path(@user)
        assert_template 'users/edit'
@@ -26,5 +26,20 @@ class UsersEditTest < ActionDispatch::IntegrationTest
        @user.reload
        assert_equal name, @user.name
        assert_equal email, @user.email
+    end
+    
+    test "successful edit with friendly forwarding" do
+       get edit_user_path(@user)
+       log_in_as(@user)
+       assert_redirected_to edit_user_url(@user)
+       name = "Foo Bar"
+       email = "foo@bar.com"
+       patch user_path(@user), params: { user: { name: name, email: email, password: "", password_confirmation: "" } }
+       assert_not flash.empty?
+       assert_redirected_to @user
+       @user.reload
+       assert_equal name, @user.name
+       assert_equal email, @user.email
+       assert_redirected_to user_path(@user)
     end
 end
