06c4a65d46184c3d22317b5e320354da1a0c6485
KSheehan <ksheehan927@gmail.com>
2017-05-31 11:37:18 -0400

    Add authentication to Activation and Sessions controllers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -29,8 +29,9 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :test
-  host = 'localhost3001' # Don't use this literally; use your local dev host instead
+  host = 'localhost:3001' # localhost
   config.action_mailer.default_url_options = { host: host, protocol: 'https' }
+    
   config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
